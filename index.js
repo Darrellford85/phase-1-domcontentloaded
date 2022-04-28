78b5135165c296baba62c645b771ba6b5e6a9f3b
Darrell Ford <Darrellford85@gmail.com>
2022-04-28 15:40:47 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", ()=>{
+    const newDOMText = document.getElementById("text")
+    return newDOMText.textContent = "This is really cool!"
+})
